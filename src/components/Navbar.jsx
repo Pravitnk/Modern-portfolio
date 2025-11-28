15f466c8842363790351c44962fc106e37a7c219
Pravit Naik <pravitnaik656@.com>
2025-11-28 22:06:45 +0530

    Enhance Navbar and OverlayMenu components with improved layout and functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import OverlayMenu from "./OverlayMenu";
-import logo from "../assets/Logo(1).png";
+import logo from "../assets/Logo (1).png";
+import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [visible, setVisible] = useState(true);
+
   return (
     <>
       <nav
@@ -12,12 +14,33 @@ const Navbar = () => {
           visible ? "translate-y-0" : "-translate-y-full"
         }`}
       >
-        <div className="">
+        <div className="flex items-center space-x-2">
           <img src={logo} alt="logo" className="w-8 h-8" />
+          <div className="text-2xl font-bold text-white hidden sm:block">
+            Pravit
+          </div>
+
+          <div className="block lg:absolute lg:left-1/2 lg:transform lg:-translate-x-1/2 h-10 w-20 bg-amber-200 rounded-full cursor-pointer shadow-lg hover:opacity-90 transition-opacity duration-500 hover:w-40 focus:w-40 focus:outline-none px-4 hover:transition-all">
+            {/* <button
+              className="text-white text-3xl focus:outline-none"
+              aria-label={menuOpen ? "Close menu" : "Open menu"}
+              onClick={() => setMenuOpen(true)}
+            >
+              <FiMenu />
+            </button> */}
+          </div>
+        </div>
+        <div className="hidden  lg:block">
+          <a
+            href="#contact"
+            className="bg-gradient-to-r from-pink-500 to-blue-500 text-white px-5 py-2 rounded-full font-medium shadow-lg hover:opacity-90 transition-opacity duration-300"
+          >
+            Reach Out
+          </a>
         </div>
       </nav>
 
-      <OverlayMenu />
+      <OverlayMenu isOpen={menuOpen} onClose={() => setMenuOpen(false)} />
     </>
   );
 };
--- src/components/OverlayMenu.jsx
+++ src/components/OverlayMenu.jsx
@@ -1,7 +1,23 @@
+import { AnimatePresence } from "framer-motion";
+import { FiXCircle } from "react-icons/fi";
+import { motion } from "framer-motion";
 import React from "react";
 
-const OverlayMenu = () => {
-  return <div>OverlayMenu</div>;
+const OverlayMenu = ({ isOpen, onClose }) => {
+  return (
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div className="fixed inset-0 bg-black bg-opacity-75 flex flex-col items-center justify-center z-50">
+          <button
+            onClick={onClose}
+            className="absolute top-5 right-5 text-white text-3xl"
+          >
+            <FiXCircle />
+          </button>
+        </motion.div>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default OverlayMenu;
